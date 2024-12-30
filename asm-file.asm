e58623c94e03032f9fe9c11fd415a7b730195cf9
[akashafatima610] <[akashafatima610@gmail.com]>
2024-12-30 12:53:49 +0500

    Akasha_Fatima_1132

--- asm-file.asm
+++ asm-file.asm
@@ -12,25 +12,30 @@ count PROC p1:DWORD, p2:DWORD
     
     push ebp
     mov ebp,esp
-    mov ecx, [ebp + 8]
-    mov eax, [ebp + 12] 
+    mov esi, [ebp + 8]
+    mov ecx, [ebp + 12] 
     mov edx, 0
 Loop_Start:
-    cmp eax, 0
+    cmp ecx, 0
     je Loop_End
-    mov esi, [ecx]
-    cmp esi, 0
+    mov eax, [esi]
+    cmp eax, 0
     jle not_Positive
     inc edx
 
 not_Positive:
-    add ecx, 4
-    dec eax
+    add esi, 4
+    dec ecx
     jmp Loop_Start
 Loop_End:
     mov eax, edx
     mov esp, ebp
     pop ebp
+
+    mov edx, OFFSET msg
+    call writestring
+    call writedec                ; Display the count of positive numbers
+    call crlf
     ret
 count ENDP
 end
